868c72dc0eb1912426b5e40af3407700989dbb15
thinktank01 <felixayot@gmail.com>
2024-02-08 00:53:51 +0300

    Modify format() in RedactingFormatter to prevent manual extrapolation of FORMAT

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -35,7 +35,7 @@ def get_logger() -> logging.Logger:
 def get_db() -> MySQLConnection:
     """Returns a connector to a MySQL database."""
     return connect(
-        username=os.getenv("PERSONAL_DATA_DB_USERNAME"),
+        user=os.getenv("PERSONAL_DATA_DB_USERNAME"),
         password=os.getenv("PERSONAL_DATA_DB_PASSWORD"),
         host=os.getenv("PERSONAL_DATA_DB_HOST"),
         database=os.getenv("PERSONAL_DATA_DB_NAME")
@@ -43,21 +43,24 @@ def get_db() -> MySQLConnection:
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ 
+        Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """Initializes the RedactingFormatter object."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """Format the record."""
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
 def main() -> None:
